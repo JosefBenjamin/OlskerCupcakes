e695536ebacf5d491978de540ce87788493db27f
Johan <179227533+JosefBenjamin@users.noreply.github.com>
2025-03-27 13:27:42 +0100

    made an orderlinemapper empty class

--- src/main/java/app/persistence/OrderLineMapper.java
+++ src/main/java/app/persistence/OrderLineMapper.java
@@ -3,7 +3,6 @@ package app.persistence;
 public class OrderLineMapper {
 
     //TODO: Make SQL joins from CakeTop and CakeBot
-    // In order to view a complete orderline and all orderlines for a given user
-
+    // In order to view a complete orderline and all orderlines for a given user okay so
 
 }
